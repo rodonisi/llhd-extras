456cb07e4d775856503f3b17c8e25e852c204b23
rodonisi <simon@rodoni.ch>
2020-08-22 12:01:18 +0200

    add cdc vcd traces

--- artifact/src/cdc_fifo_gray_tb.sv
+++ artifact/src/cdc_fifo_gray_tb.sv
@@ -5,17 +5,17 @@ module cdc_fifo_gray_tb;
 
   logic src_rst_ni = 1'b1;
   logic src_clk_i;
-  int   src_data_i;
+  bit[31:0]   src_data_i;
   logic src_valid_i;
   logic src_ready_o;
 
   logic dst_rst_ni = 1'b1;
   logic dst_clk_i;
-  int   dst_data_o;
+  bit[31:0]   dst_data_o;
   logic dst_valid_o;
   logic dst_ready_i;
 
-  cdc_fifo_gray #(.T(int), .LOG_DEPTH(3)) dut (
+  cdc_fifo_gray #(.T(bit[31:0]), .LOG_DEPTH(3)) dut (
     .src_rst_ni(src_rst_ni),
     .src_clk_i(src_clk_i),
     .src_data_i(src_data_i),
