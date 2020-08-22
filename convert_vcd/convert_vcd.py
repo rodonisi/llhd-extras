bdbfe7d6722fa2bcbd96ea57de4df84dd6ef4879
rodonisi <simon@rodoni.ch>
2020-08-22 17:01:21 +0200

    fix array conversion

--- convert_vcd/convert_vcd.py
+++ convert_vcd/convert_vcd.py
@@ -4,6 +4,9 @@ import math
 import re
 import sys
 import argparse
+from pprint import PrettyPrinter
+
+p = PrettyPrinter()
 
 # Construct the argument parser
 ap = argparse.ArgumentParser()
@@ -23,23 +26,35 @@ if args.output != '-' :
 vcd = VCDVCD(args.input)
 data = vcd.get_data()
 
-
-def format_value_dump(val, size):
+def format_value_dump(val, size, n_elems):
     """ Formats the value string to a hexadecimal representation, 0-padded to the next byte.
 
     Args:
         val (str): the string representing the value to format
-        size (str): the bitwidth of val
+        size (int): the bitwidth of val
+        n_elems (int): the number of elements, required to pretty_print arrays. An array has the number of n_elems < size
     Returns:
         The formatted value. If the value is not numeric (e.g. an 'x'), val is returned as is.
     """
-    if val.isdigit():
-        words = math.ceil(int(size)/8) * 2
-        hexa = vcd._to_hex(val)
-        hex_words = len(hexa)
-        leading = '0x' + ('0' * (words - hex_words))
-        return leading + hexa
-    return val
+    digit = val.isdigit()
+    words = int(math.ceil(size/8) * 2)
+    hexa = vcd._to_hex(val)
+    hex_words = len(hexa)
+    zext = ('0' * (words - hex_words)) + hexa
+    if n_elems > 0 and n_elems < size:
+        elem_word = int(words/n_elems)
+        elem_format = []
+        for i in range(0, n_elems):
+            if digit:
+                start = i*elem_word
+                elem_format.append('0x' + zext[start:(start + elem_word)])
+            else:
+                elem_format.append(val)
+        elem_format.reverse()
+        return elem_format
+    if digit:
+        return ['0x' + zext]
+    return [val]
 
 
 # Transpose the signal changes to be ordered by time-step. Changes equal to the
@@ -49,14 +64,29 @@ lastValue = {}
 
 for i in data:
     sig = data[i]
+
+    elem_size = -1
+    s = re.split(r'(\[\d+:\d+\])', sig['references'][0])
+    if len(s) > 1:
+        v = re.split(r'\D+', s[1])
+        elem_size = int(v[1]) + 1 - int(v[2])
+
     name_format = re.sub('\[\d+:\d+\]', '', sig['references'][0])
     name_format = re.sub('\.', '/', name_format)
     for change in sig['tv']:
-        if i not in lastValue or lastValue[i] != change[1]:
-            val_format = format_value_dump(change[1], sig['size'])
-            dump_format = '  ' + name_format + '  ' + val_format
-            changes.setdefault(change[0], []).append(dump_format)
-        lastValue[i] = change[1]
+        val_formats = format_value_dump(change[1], int(sig['size']), elem_size)
+        name_formats = []
+        if len(val_formats) > 1:
+            for x in range(0,len(val_formats)):
+                name_formats.append(name_format + '[' + str(x) + ']')
+        else:
+            name_formats = [name_format]
+        for c in zip(name_formats, val_formats):
+            n, v = c
+            if n not in lastValue or lastValue[n] != v:
+                dump_format = '  ' + n + '  ' + v
+                changes.setdefault(change[0], []).append(dump_format)
+            lastValue[n] = v
 
 # Dump the trace in a diff-able format.
 for time in sorted(changes.keys()):
